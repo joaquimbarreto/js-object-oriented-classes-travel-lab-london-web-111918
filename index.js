99b0acb521152f8debaf31aac9b37ea5ae0a460e
Joaquim Barreto <joaquim@barreto.co>
2019-01-13 19:32:41 +0000

    not going to NYC

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+class Driver{
+    
+    constructor(name, startDate) {
+        this.name = name;
+        this.startDate = new Date( startDate );
+    }
 
+    yearsExperienceFromBeginningOf(year) {
+        let endDate = new Date(year);
+        return endDate.getFullYear() - this.startDate.getFullYear();
+    }
+        
+}
+
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
+  
+class Route {
+
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+    }
+
+}
